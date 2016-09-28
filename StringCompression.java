15bde84cfa8887ac5993d370f20226f75985e3af
aarav malik <aaravmalik0610@gmail.com>
2016-09-27 20:45:57 -0400

    addition to String Compression

--- StringCompression.java
+++ StringCompression.java
@@ -18,3 +18,20 @@ String compress(String str){
     }
     return compressed.length()>=str.length()?str:compressed.toString();
 }
+
+//We could also calculate length of the compressed string before we actually compute the compressed string
+
+int countCompression(String str){
+
+    int compressedLength = 0
+    int countConsecutive = 0;
+    for(int i = 0; i<str.length(); i++){
+        countConsecutive++;
+        if(i+1>str.length() || str.charAt(i) != str.charAt(i+1)){
+            compressedLength += 1 + String.valueOf(countConsecutive).length();
+            countConsecutive = 0;
+        }
+    }
+    return compresedLength;
+
+}
