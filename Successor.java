bca48da4de468de98217f0c73d54f92b2e2b5ac5
aarav malik <aaravmalik0610@gmail.com>
2016-10-11 20:57:59 -0400

    Successor

---
+++ Successor.java
@@ -0,0 +1,21 @@
+TreeNode inorderSucc(TreeNode n){
+    if(n == null) return null;
+    if(n.right != null) return leftMostChild(n.right);
+    else{
+        TreeNode q = n;
+        TreeNode x = n.parent;
+        while(x!=null x.left != q){
+            q=x;
+            x=x.parent;
+        }
+     return x;   
+    }
+}
+
+TreeNode leftMostChild(TreeNode n){
+    if (n==null) return null;
+    while(n.left != null){
+        n = n.left;
+    }
+    return n;
+}
